fed2ff67b35ddd56eb2aa12f94e5b0c0a422b606
Tronky <john.kosinski@gmail.com>
2024-11-20 00:28:51 +0700

    Security Context tests

--- test/SecurityContext.ts
+++ test/SecurityContext.ts
@@ -3,35 +3,84 @@ import { expect } from 'chai';
 import hre from 'hardhat';
 import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
 
+const ADMIN_ROLE='0x0000000000000000000000000000000000000000000000000000000000000000';
+
 describe('SecurityContext', function () {
     let securityContext: any;
-    let owner: HardhatEthersSigner;
-    let nonOwner: HardhatEthersSigner;
+    let admin: HardhatEthersSigner;
+    let nonAdmin1: HardhatEthersSigner;
+    let nonAdmin2: HardhatEthersSigner;
 
     this.beforeEach(async () => {
-        const [addr1, addr2] = await hre.ethers.getSigners();
-        owner = addr1;
-        nonOwner = addr2;
+        const [a1, a2, a3] = await hre.ethers.getSigners();
+        admin = a1;
+        nonAdmin1 = a2;
+        nonAdmin2 = a3;
 
         const SecurityContextFactory =
             await hre.ethers.getContractFactory('SecurityContext');
-        securityContext = await SecurityContextFactory.deploy(owner.address);
+        securityContext = await SecurityContextFactory.deploy(admin.address);
     });
 
     describe('Deployment', function () {
         it('Should set the right owner', async function () {
             expect(
                 await securityContext.hasRole(
-                    '0x0000000000000000000000000000000000000000000000000000000000000000',
-                    owner.address
+                    ADMIN_ROLE,
+                    admin.address
                 )
             ).to.be.true;
             expect(
                 await securityContext.hasRole(
-                    '0x0000000000000000000000000000000000000000000000000000000000000000',
-                    nonOwner.address
+                    ADMIN_ROLE,
+                    nonAdmin1.address
                 )
             ).to.be.false;
+            expect(
+                await securityContext.hasRole(
+                    ADMIN_ROLE,
+                    nonAdmin2.address
+                )
+            ).to.be.false;
+        });
+    });
+
+
+    describe("Transfer Adminship", function () {
+        it("can grant admin to self", async function () {
+            await securityContext.grantRole(ADMIN_ROLE, admin.address);
+
+            expect(await securityContext.hasRole(ADMIN_ROLE, admin.address)).to.be.true;
+            expect(await securityContext.hasRole(ADMIN_ROLE, nonAdmin1.address)).to.be.false;
+            expect(await securityContext.hasRole(ADMIN_ROLE, nonAdmin2.address)).to.be.false;
+        });
+
+        it("can transfer admin to another", async function () {
+            await securityContext.grantRole(ADMIN_ROLE, nonAdmin1.address);
+
+            //now there are two admins
+            expect(await securityContext.hasRole(ADMIN_ROLE, admin.address)).to.be.true;
+            expect(await securityContext.hasRole(ADMIN_ROLE, nonAdmin1.address)).to.be.true;
+            expect(await securityContext.hasRole(ADMIN_ROLE, nonAdmin2.address)).to.be.false;
+
+            await securityContext.connect(nonAdmin1).revokeRole(ADMIN_ROLE, admin.address);
+
+            //now origin admin has had adminship revoked 
+            expect(await securityContext.hasRole(ADMIN_ROLE, admin.address)).to.be.false;
+            expect(await securityContext.hasRole(ADMIN_ROLE, nonAdmin1.address)).to.be.true;
+            expect(await securityContext.hasRole(ADMIN_ROLE, nonAdmin2.address)).to.be.false;
+        });
+
+        it("can pass adminship along", async function () {
+            await securityContext.grantRole(ADMIN_ROLE, nonAdmin1.address);
+            await securityContext.connect(nonAdmin1).revokeRole(ADMIN_ROLE, admin.address);
+            await securityContext.connect(nonAdmin1).grantRole(ADMIN_ROLE, nonAdmin2.address);
+            await securityContext.connect(nonAdmin2).revokeRole(ADMIN_ROLE, nonAdmin1.address);
+
+            //in the end, adminship has passed from admin to nonAdmin1 to nonAdmin2
+            expect(await securityContext.hasRole(ADMIN_ROLE, admin.address)).to.be.false;
+            expect(await securityContext.hasRole(ADMIN_ROLE, nonAdmin1.address)).to.be.false;
+            expect(await securityContext.hasRole(ADMIN_ROLE, nonAdmin2.address)).to.be.true;
         });
     });
 });
