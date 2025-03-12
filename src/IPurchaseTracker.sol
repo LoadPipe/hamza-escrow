3d64c14cf78cbaa3928e269e8ccef5380cd370f3
Tronky <john.kosinski@gmail.com>
2025-03-12 21:53:25 +0700

    expanded IPurchaseTracker for CommunityVault refactoring

--- src/IPurchaseTracker.sol
+++ src/IPurchaseTracker.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.20;
 
 interface IPurchaseTracker {
     function recordPurchase(bytes32 paymentId, address seller, address buyer, uint256 amount) external;
+    function getPurchaseCount(address recipient) external view returns (uint256);
+    function getPurchaseAmount(address recipient) external view returns (uint256);
+    function getSalesCount(address recipient) external view returns (uint256);
+    function getSalesAmount(address recipient) external view returns (uint256);
 }
