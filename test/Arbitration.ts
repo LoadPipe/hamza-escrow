8da973b27a3c87a88386d655c1867322b32c24c7
jrkosinski <john.kosinski@gmail.com>
2025-07-27 01:23:53 +0700

    framework for arbitration tests

---
+++ test/Arbitration.ts
@@ -0,0 +1,146 @@
+import { expect } from 'chai';
+import hre, { ethers } from 'hardhat';
+import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
+import { BigNumberish, keccak256 } from 'ethers';
+import { IEscrow, convertEscrow as convertEscrow } from './util';
+
+describe('Arbitration', function () {
+    let securityContext: any;
+    let systemSettings: any;
+    let polyEscrow: any;
+    let testToken: any;
+    let arbitrationModule: any;
+    let admin: HardhatEthersSigner;
+    let nonOwner: HardhatEthersSigner;
+    let payer1: HardhatEthersSigner;
+    let payer2: HardhatEthersSigner;
+    let receiver1: HardhatEthersSigner;
+    let receiver2: HardhatEthersSigner;
+    let vaultAddress: HardhatEthersSigner;
+    let arbiter1: HardhatEthersSigner;
+    let arbiter2: HardhatEthersSigner;
+
+    this.beforeEach(async () => {
+        const [a1, a2, a3, a4, a5, a6, a7, a8, a9] =
+            await hre.ethers.getSigners();
+        admin = a1;
+        nonOwner = a2;
+        vaultAddress = a3;
+        payer1 = a4;
+        payer2 = a5;
+        receiver1 = a6;
+        receiver2 = a7;
+        arbiter1 = a8;
+        arbiter2 = a9;
+
+        //deploy security context
+        const SecurityContextFactory =
+            await hre.ethers.getContractFactory('SecurityContext');
+        securityContext = await SecurityContextFactory.deploy(admin.address);
+
+        //deploy system settings
+        const SystemSettingsFactory =
+            await hre.ethers.getContractFactory('SystemSettings');
+        systemSettings = await SystemSettingsFactory.deploy(
+            securityContext.target,
+            admin.address,
+            0
+        );
+
+        //deploy test token
+        const TestTokenFactory =
+            await hre.ethers.getContractFactory('TestToken');
+        testToken = await TestTokenFactory.deploy('XYZ', 'ZYX');
+
+        //deploy arbitration module
+        const ArbitrationModuleFactory =
+            await hre.ethers.getContractFactory('ArbitrationModule');
+        arbitrationModule = await ArbitrationModuleFactory.deploy();
+
+        //deploy polyEscrow
+        const PolyEscrowFactory =
+            await hre.ethers.getContractFactory('PolyEscrow');
+        polyEscrow = await PolyEscrowFactory.deploy(
+            securityContext.target,
+            systemSettings.target,
+            arbitrationModule.target
+        );
+
+        //grant token
+        await testToken.mint(nonOwner, 10000000000);
+        await testToken.mint(payer1, 10000000000);
+        await testToken.mint(payer2, 10000000000);
+    });
+
+    describe('Deployment', function () {
+        describe('Happy Paths', function () {
+            it('can deploy with valid arbitration module', async function () {});
+        });
+        describe('Exceptions', function () {
+            it('cannot deploy with a zero-address arbitration module', async function () {});
+            it('cannot deploy with an invalid arbitration module', async function () {});
+        });
+    });
+
+    describe('Escrow Creation', function () {
+        describe('Happy Paths', function () {
+            it('can create escrow with valid custom arbitration module', async function () {});
+            it('can create escrow with valid default arbitration module', async function () {});
+        });
+        describe('Exceptions', function () {
+            it('cannot create escrow with a zero-address arbitration module', async function () {});
+            it('cannot create escrow with an invalid arbitration module', async function () {});
+        });
+        describe('Events', function () {});
+    });
+
+    describe('Proposing Arbitration', function () {
+        describe('Happy Paths', function () {
+            it('payer can propose arbitration', async function () {});
+            it('receiver can propose arbitration', async function () {});
+            it('proposal is accepted when votes over threshold', async function () {});
+            it('proposal is rejected when votes under threshold', async function () {});
+            it('single-arbiter proposal is voted, accepted automatically on proposal creation', async function () {});
+        });
+        describe('Exceptions', function () {
+            it('stranger cannot propose arbitration', async function () {});
+            it('arbiter cannot propose arbitration', async function () {});
+            it('cannot propose arbitration on invalid escrow id', async function () {});
+            it('cannot vote on invalid proposal id', async function () {});
+            it('cannot vote on inactive proposal', async function () {});
+        });
+        describe('Events', function () {
+            it('arbitration proposal emits ArbitrationProposed', async function () {});
+        });
+    });
+
+    describe('Voting on Arbitration', function () {
+        describe('Happy Paths', function () {
+            it('arbiters can vote yes on arbitration', async function () {});
+            it('arbiters can vote no on arbitration', async function () {});
+        });
+        describe('Exceptions', function () {
+            it('payer cannot vote on arbitration', async function () {});
+            it('receiver cannot vote on arbitration', async function () {});
+            it('stranger cannot vote on arbitration', async function () {});
+        });
+        describe('Events', function () {
+            it('voting emits VoteRecorded', async function () {});
+        });
+    });
+
+    describe('Cancelling Arbitration', function () {
+        describe('Happy Paths', function () {});
+        describe('Exceptions', function () {
+            it('cannot cancel invalid proposal', async function () {});
+            it('cannot cancel inactive proposal', async function () {});
+        });
+        describe('Events', function () {});
+    });
+
+    describe('Executing Arbitration', function () {
+        describe('Happy Paths', function () {});
+        describe('Exceptions', function () {});
+        describe('Events', function () {});
+    });
+});
