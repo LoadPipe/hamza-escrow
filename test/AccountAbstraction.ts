4a923e96bb6191e38ffe2ae1efdf42ac693ec40e
jrkosinski <john.kosinski@gmail.com>
2025-08-04 21:21:05 +0700

    debugging gas stuff

--- test/AccountAbstraction.ts
+++ test/AccountAbstraction.ts
@@ -45,6 +45,13 @@ describe.only('AccountAbstraction', function () {
     });
 
     describe('User Operations', function () {
+        async function fundGas(from: HardhatEthersSigner, to: string) {
+            const transaction = await from.sendTransaction({
+                to,
+                value: hre.ethers.parseEther('1'),
+            });
+        }
+
         it.only('can send user operation', async function () {
             const sender = await hre.ethers.getCreateAddress({
                 from: accountFactory.target,
@@ -88,25 +95,41 @@ describe.only('AccountAbstraction', function () {
             console.log('SENDER:', sender);
             console.log('EntryPoint:', entryPoint.target);
 
+            //fund gas
+            await fundGas(
+                signer0,
+                '0xCafac3dD18aC6c6e92c921884f9E4176737C052c'
+            );
+
             //create the user op
             const userOp: any = {
                 sender,
                 nonce,
                 initCode,
                 callData,
+                callGasLimit: 200_000_000,
                 accountGasLimits:
-                    '0x00000000000000000000000bebc2000000000000000000000000000000000000',
+                    '0x0000000000000000000020000000000000000000000000000000000000200000',
                 preVerificationGas: 200_000_000,
                 gasFees:
-                    '0x00000000000000000000000bebc2000000000000000000000000000000000000',
+                    '0x0000000000000000000000000002000000000000000000000000000000200000',
                 paymasterAndData: '0x',
+                //paymasterAccount.address +
+                //'0000000000000000000000000000000000000000000000000000000000030d40' + // 200000 verification gas
+                //'000000000000000000000000000000000000000000000000000000000000c350', // 50000 postop gas
                 signature: '0x',
             };
 
             console.log(userOp);
+            await entryPoint.depositTo(paymasterAccount.address, {
+                value: ethers.parseEther('10'),
+            });
 
             //send the userop
-            const tx = await entryPoint.handleOps([userOp], signer0.address);
+            const tx = await entryPoint.handleOps(
+                [userOp],
+                paymasterAccount.address
+            );
             const receipt = await tx.wait();
 
             //TODO: find a better way to get the address
@@ -126,17 +149,32 @@ describe.only('AccountAbstraction', function () {
                 nonce: await entryPoint.getNonce(sender, 0),
                 initCode: '0x',
                 callData,
+                callGasLimit: 500_000, // Reasonable for most operations
                 accountGasLimits:
-                    '0x00000000000000000000000bebc2000000000000000000000000000000000000',
-                preVerificationGas: 200_000_000,
+                    '0x' +
+                    (100_000).toString(16).padStart(64, '0') + // verificationGasLimit
+                    (500_000).toString(16).padStart(64, '0'), // callGasLimit (matches above)
+                preVerificationGas: 50_000, // Reasonable pre-verification
                 gasFees:
-                    '0x00000000000000000000000bebc2000000000000000000000000000000000000',
+                    '0x' +
+                    (2_000_000_000).toString(16).padStart(32, '0') + // maxPriorityFeePerGas (2 gwei)
+                    (20_000_000_000).toString(16).padStart(32, '0'), // maxFeePerGas (20 gwei)
                 paymasterAndData: '0x',
                 signature: '0x',
             };
 
             console.log(userOp2);
-            const tx2 = await entryPoint.handleOps([userOp2], signer0.address);
+            (
+                await entryPoint.depositTo(paymasterAccount.address, {
+                    value: ethers.parseEther('10'),
+                })
+            ).wait();
+            return;
+
+            const tx2 = await entryPoint.handleOps(
+                [userOp2],
+                paymasterAccount.address
+            );
             const receipt2 = await tx2.wait();
 
             console.log('counter:', await account.getCounter());
@@ -147,16 +185,23 @@ describe.only('AccountAbstraction', function () {
                 nonce: await entryPoint.getNonce(sender, 0),
                 initCode: '0x',
                 callData,
+                callGasLimit: 200_000_000,
                 accountGasLimits:
-                    '0x00000000000000000000000bebc2000000000000000000000000000000000000',
+                    '0x0000000000000100bebc20000000000000000000000000000000000000002000',
                 preVerificationGas: 200_000_000,
                 gasFees:
-                    '0x00000000000000000000000bebc2000000000000000000000000000000000000',
+                    '0x0000000000000100bebc2000000000000000000000000000000000000002000',
                 paymasterAndData: '0x',
                 signature: '0x',
             };
 
             console.log(userOp3);
+            (
+                await entryPoint.depositTo(paymasterAccount.address, {
+                    value: ethers.parseEther('10'),
+                })
+            ).wait();
+
             const tx3 = await entryPoint.handleOps([userOp3], signer0.address);
             const receipt3 = await tx3.wait();
 
